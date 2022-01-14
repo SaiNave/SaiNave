00b2621ac6eef284958e15818bf10868f513a099
SaiNave <nave.sai25@gmail.com>
2022-01-13 20:29:22 -0500

    adding test files

---
+++ OOP_in_Python/class_python.py
@@ -0,0 +1,16 @@
+class class_python:
+    pass # it means this class has no methods (or functions)
+
+
+emp1 = class_python()
+emp2 = class_python()
+
+#print(emp1,"\n",emp2)
+
+emp1.first = input("Enter 1st Employee first name:")
+emp1.last = input("Enter 1st Employee second name:")
+emp1.email = input("Enter 1st Employee email address:")
+
+emp2.first = input("Enter 2nd Employee first name:")
+emp2.last = input("Enter 2nd Employee second name:")
+emp2.email = input("Enter 2nd Employee email address:")
